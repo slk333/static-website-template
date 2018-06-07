178189b61cc233a50ad754528dd9af8c530b6138
Antoine Weber <aweb33@icloud.com>
2018-06-07 19:14:44 +0200

    remove css from JS, use media query for everything. Hamburger only switch display

--- template.js
+++ template.js
@@ -1,5 +1,5 @@
 var hamburger = document.querySelector("#hamburger");
-var rightHeader = document.querySelector("#rightHeader");
+var rightHeader = document.querySelector(".rightHeader");
 var article = document.querySelector("article");
 var footer = document.querySelector("footer");
 
@@ -9,15 +9,48 @@ var footer = document.querySelector("footer");
 hamburger.onclick = function(){
 if (hamburger.textContent == "三"){
 hamburger.textContent = "X";
-rightHeader.setAttribute("style","display:grid;grid-template-columns:repeat(1,1fr);");
+rightHeader.style.display = "grid"  // afficher les liens
+  //rightHeader.classList.add('mobile-rightHeader');
+  //rightHeader.classList.remove('rightHeader');
+ //rightHeader.classList.remove('display-none');
 article.setAttribute("style","display:none");
 footer.setAttribute("style","display:none");
 }
 else {
-rightHeader.setAttribute("style","display:none;");
+  //rightHeader.classList.add('display-none');
+ //rightHeader.classList.remove('rightHeader');
+ //rightHeader.classList.remove('mobile-rightHeader');
 hamburger.textContent = "三";
+rightHeader.style.display = "none" // masquer les liens
 article.style.display = "block";
 footer.style.display = "block";
 }
+
+}
+
+
+
+// media query event handler
+if (matchMedia) {
+  // const mq = window.matchMedia("(min-width: 700px)");
+  // mq.addListener(WidthChange);
+  // WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+  if (mq.matches) {
+
+	 //   rightHeader.classList.add('rightHeader');
+	  //rightHeader.classList.remove('mobile-rightHeader');
+
+   // window width is > 700px
+    
+  } else {
+	// window width is < 700px
+	      
+	      
+
+  }
 
 }
